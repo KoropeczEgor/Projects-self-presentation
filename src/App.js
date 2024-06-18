b862b5322fe19a1dd9353a987a63ce8cf63fcc64
Egor <koropeczegor@gmail.com>
2024-06-18 17:38:54 +0300

    Contacts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Banner from "./components/banner/index";
+import Contact from "./components/contact/index";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+
+      <Contact />
     </div>
   );
 }
