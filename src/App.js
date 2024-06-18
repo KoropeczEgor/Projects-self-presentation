c2e327c9d67f84446a7ba04ad2f0cf5f61b3b62d
Egor <koropeczegor@gmail.com>
2024-06-18 23:33:58 +0300

    footer creat#1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from "./components/banner";
 import Contact from "./components/contact";
 import Skills from "./components/skills";
 import Projects from "./components/projects";
+import Footer from "./components/footer";
 
 import "./bootstrap-4.0.0-dist/css/bootstrap.min.css";
 
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
