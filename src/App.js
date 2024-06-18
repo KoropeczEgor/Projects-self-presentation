f6aa70303cb635dceb9e815a6e18bf023cdac3ec
Egor <koropeczegor@gmail.com>
2024-06-18 18:28:28 +0300

    navBar create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Banner from "./components/banner/index";
-import Contact from "./components/contact/index";
+import NavBar from "./components/navBar";
+import Banner from "./components/banner";
+import Contact from "./components/contact";
+import "./bootstrap-4.0.0-dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Banner />
 
       <Contact />
